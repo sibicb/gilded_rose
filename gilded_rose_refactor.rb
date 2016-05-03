e5239e3f1c4a75854ff8b1974b63ce061199dac6
Jim Ryan Zulueta <jimryanzulueta@gmail.com>
2016-05-04 00:12:15 +0800

    further refactored

--- gilded_rose_refactor.rb
+++ gilded_rose_refactor.rb
@@ -1,41 +1,48 @@
 class GildedRose
+  attr_accessor :item
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
+      update_sell_in(item)
+
       case item.name
       when 'Aged Brie'
-        item.quality = 50 if item.quality > 50
-        item.quality += 1 if item.sell_in > 0
-        item.sell_in -= 1
+        item.quality += 1
+        validate_quality(item)
 
       when 'Sulfuras, Hand of Ragnaros'
         item.quality = 80
 
       when 'Backstage passes to a TAFKAL80ETC concert'
-        item.quality = 50 if item.quality > 50
         item.quality += 1 if item.sell_in > 10
-        item.quality += 2 if item.sell_in > 5 && item.sell_in < 10
+        item.quality += 2 if item.sell_in > 5 && item.sell_in <= 10
         item.quality += 3 if item.sell_in <= 5 && item.sell_in > 0
-        item.quality = 0 if item.sell_in == 0
-        item.sell_in -= 1
+        validate_quality(item)
 
       when 'Conjured'
-        item.quality = 50 if item.quality > 50
         item.quality -= 2 if item.quality >= 0 && item.sell_in >= 0
         item.quality -= 4 if item.quality >= 0 && item.sell_in < 0
-        item.quality = 0 if item.quality < 0
-        item.sell_in -= 1
+        validate_quality(item)
 
       else
-      	item.quality = 50 if item.quality > 50
-      	sell_in > 0 ? item.quality -= 1 : item.quality -= 2
-      	item.quality = 0 if item.quality < 0
+        sell_in > 0 ? (item.quality -= 1) : (item.quality -= 2)
+        validate_quality(item)
+
       end
-    puts "#{item.name}, #{item.sell_in}, #{item.quality}"
+      puts "#{item.name}, #{item.sell_in}, #{item.quality}"
     end
+  end
+
+  def update_sell_in(item)
+    item.sell_in -= 1
+  end
+
+  def validate_quality(item)
+    item.quality = 50 if item.quality >= 50
+    item.quality = 0 if item.quality < 0
   end
 end
 
