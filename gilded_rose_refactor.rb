24399e71928ea72bb72c7b70869b158250886690
Jim Ryan Zulueta <jimryanzulueta@gmail.com>
2016-05-03 23:26:28 +0800

    refactor code

--- gilded_rose_refactor.rb
+++ gilded_rose_refactor.rb
@@ -6,32 +6,35 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       case item.name
-        when "Aged Brie" 
-          item.quality = 50 if item.quality > 50
-          item.quality += 1 if item.sell_in > 0 
-          item.sell_in -= 1
+      when 'Aged Brie'
+        item.quality = 50 if item.quality > 50
+        item.quality += 1 if item.sell_in > 0
+        item.sell_in -= 1
 
-        #when "Sulfuras, Hand of Ragnaros"
-        when "Sulfuras" 
-          item.quality = 80
+      when 'Sulfuras, Hand of Ragnaros'
+        item.quality = 80
 
-        #when "Backstage passes to a TAFKAL80ETC con"
-        when "Backstage passes"
-          item.quality = 50 if item.quality > 50
-          item.quality += 1 if item.sell_in > 10
-          item.quality += 2 if item.sell_in > 5 && item.sell_in < 10
-          item.quality += 3 if item.sell_in <= 5 && item.sell_in > 0
-          item.quality = 0 if item.sell_in == 0
-          item.sell_in -= 1
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        item.quality = 50 if item.quality > 50
+        item.quality += 1 if item.sell_in > 10
+        item.quality += 2 if item.sell_in > 5 && item.sell_in < 10
+        item.quality += 3 if item.sell_in <= 5 && item.sell_in > 0
+        item.quality = 0 if item.sell_in == 0
+        item.sell_in -= 1
 
-        #when "Conjured Mana Cake"
-        when "Conjured"
-          item.quality = 50 if item.quality > 50
-          item.quality -= 2 if item.quality >= 0 && item.sell_in >= 0
-          item.quality -= 4 if item.quality >= 0 && item.sell_in < 0
-          item.quality = 0 if item.quality < 0
-          item.sell_in -= 1
+      when 'Conjured'
+        item.quality = 50 if item.quality > 50
+        item.quality -= 2 if item.quality >= 0 && item.sell_in >= 0
+        item.quality -= 4 if item.quality >= 0 && item.sell_in < 0
+        item.quality = 0 if item.quality < 0
+        item.sell_in -= 1
+
+      else
+      	item.quality = 50 if item.quality > 50
+      	sell_in > 0 ? item.quality -= 1 : item.quality -= 2
+      	item.quality = 0 if item.quality < 0
       end
+    puts "#{item.name}, #{item.sell_in}, #{item.quality}"
     end
   end
 end
@@ -45,7 +48,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
