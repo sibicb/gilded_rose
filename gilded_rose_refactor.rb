cb268d1b5ba8de49e86e962d229b35c1f03a4f08
Jim Ryan Zulueta <jimryanzulueta@gmail.com>
2016-05-04 00:59:22 +0800

    refactored further

--- gilded_rose_refactor.rb
+++ gilded_rose_refactor.rb
@@ -4,35 +4,12 @@ class GildedRose
     @items = items
   end
 
-  def update_quality
+  def update
     @items.each do |item|
       update_sell_in(item)
-
-      case item.name
-      when 'Aged Brie'
-        item.quality += 1
-        validate_quality(item)
-
-      when 'Sulfuras, Hand of Ragnaros'
-        item.quality = 80 # ensures Sulfuras is on top quality all the time
-
-      when 'Backstage passes to a TAFKAL80ETC concert'
-        item.quality += 1 if item.sell_in > 10
-        item.quality += 2 if item.sell_in > 5 && item.sell_in <= 10
-        item.quality += 3 if item.sell_in <= 5 && item.sell_in > 0
-        validate_quality(item)
-
-      when 'Conjured'
-        item.quality -= 2 if item.quality >= 0 && item.sell_in >= 0
-        item.quality -= 4 if item.quality >= 0 && item.sell_in < 0
-        validate_quality(item)
-
-      else
-        sell_in > 0 ? (item.quality -= 1) : (item.quality -= 2)
-        validate_quality(item)
-
-      end
-      puts "#{item.name}, #{item.sell_in}, #{item.quality}"
+      update_quality(item)
+      validate_quality(item)
+      info(item)
     end
   end
 
@@ -41,8 +18,33 @@ class GildedRose
   end
 
   def validate_quality(item)
+  	if item.name != 'Sulfuras, Hand of Ragnaros'
     item.quality = 50 if item.quality >= 50
     item.quality = 0 if item.quality < 0
+    end
+  end
+
+  def info(item)
+    puts "Name: #{item.name}, Must be sold in: #{item.sell_in} Days,
+    Quality: #{item.quality}"
+  end
+
+  def update_quality(item)
+    case item.name
+    when 'Aged Brie'
+      item.quality += 1
+    when 'Sulfuras, Hand of Ragnaros'
+      item.quality = 80 # ensures Sulfuras is on top quality all the time
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      item.quality += 1 if item.sell_in > 10
+      item.quality += 2 if item.sell_in > 5 && item.sell_in <= 10
+      item.quality += 3 if item.sell_in <= 5 && item.sell_in > 0
+    when 'Conjured'
+      item.quality -= 2 if item.quality >= 0 && item.sell_in >= 0
+      item.quality -= 4 if item.quality >= 0 && item.sell_in < 0
+    else
+      sell_in > 0 ? (item.quality -= 1) : (item.quality -= 2)
+    end
   end
 end
 
