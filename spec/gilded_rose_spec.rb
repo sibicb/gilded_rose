cb32c42da8eb9fe454d6f51aafd0c423b471d658
Jim Ryan Zulueta <jimryanzulueta@gmail.com>
2016-05-04 10:24:29 +0800

    added initial tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -7,12 +7,20 @@ RSpec.configure do |config|
   config.color = true
 end
 
-before :all do
-        list_of_items = [
-            Item.new("Aged Brie", 5, 48),
-            Item.new("Sulfuras, Hand of Ragnaros", 5, 80),
-            Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 1),
-            Item.new("Conjured", 10, 20),
-            Item.new("Lame Item", 6, 5)
-        ]
+describe Item do
+  context 'stores 5 items in the array variable' do
+    let(:list_of_items) { [
+        Item.new("Aged Brie", 5, 48),
+        Item.new("Sulfuras, Hand of Ragnaros", 5, 80),
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 1),
+        Item.new("Conjured", 10, 20),
+        Item.new("Lame Item", 6, 5)
+    ] }
+    it 'returns count 5' do
+      expect(list_of_items.count).to eq(5)
+    end
+    it 'is an array object' do
+      expect(list_of_items.kind_of?(Array)).to eq(true)
+    end
+  end
 end
