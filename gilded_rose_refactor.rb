fb8c7f014d376559363b1c596d6fa91c29ef76b9
Jim Ryan Zulueta <jimryanzulueta@gmail.com>
2016-05-04 09:51:34 +0800

    Reworked logic and started on rspec

--- gilded_rose_refactor.rb
+++ gilded_rose_refactor.rb
@@ -8,25 +8,8 @@ class GildedRose
     @items.each do |item|
       update_sell_in(item)
       update_quality(item)
-      validate_quality(item)
       info(item)
     end
-  end
-
-  def update_sell_in(item)
-    item.sell_in -= 1
-  end
-
-  def validate_quality(item)
-  	if item.name != 'Sulfuras, Hand of Ragnaros'
-    item.quality = 50 if item.quality >= 50
-    item.quality = 0 if item.quality < 0
-    end
-  end
-
-  def info(item)
-    puts "Name: #{item.name}, Must be sold in: #{item.sell_in} Days,
-    Quality: #{item.quality}"
   end
 
   def update_quality(item)
@@ -39,18 +22,35 @@ class GildedRose
       item.quality += 1 if item.sell_in > 10
       item.quality += 2 if item.sell_in > 5 && item.sell_in <= 10
       item.quality += 3 if item.sell_in <= 5 && item.sell_in > 0
+      item.quality = 0 if item.sell_in == 0
     when 'Conjured'
       item.quality -= 2 if item.quality >= 0 && item.sell_in >= 0
       item.quality -= 4 if item.quality >= 0 && item.sell_in < 0
     else
-      sell_in > 0 ? (item.quality -= 1) : (item.quality -= 2)
+      item.sell_in > 0 ? (item.quality -= 1) : (item.quality -= 2)
     end
+    validate_quality(item)
+  end
+
+  def update_sell_in(item)
+    item.sell_in -= 1
+  end
+
+  def validate_quality(item)
+    if item.name != 'Sulfuras, Hand of Ragnaros'
+      item.quality = 50 if item.quality >= 50
+      item.quality = 0 if item.quality <= 0
+    end
+  end
+
+  def info(item)
+    puts "Name: #{item.name},
+    Must be sold in: #{item.sell_in} Days, Quality: #{item.quality}"
   end
 end
 
 class Item
   attr_accessor :name, :sell_in, :quality
-
   def initialize(name, sell_in, quality)
     @name = name
     @sell_in = sell_in
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
-require File.join(File.dirname(__FILE__) + '/..', 'gilded_rose')
+require_relative '../gilded_rose.rb'
 
-describe GildedRose do
+RSpec.configure do |config|
+  config.failure_color = :magenta
+  config.tty = true
+  config.color = true
+end
 
+before :all do
+        list_of_items = [
+            Item.new("Aged Brie", 5, 48),
+            Item.new("Sulfuras, Hand of Ragnaros", 5, 80),
+            Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 1),
+            Item.new("Conjured", 10, 20),
+            Item.new("Lame Item", 6, 5)
+        ]
 end
